e9667a8f7fd637aa31d39dc05bbd9cf0748d0ef0
Joshua LeBlanc <jleblanc@omics.com>
2018-04-25 20:47:33 -0300

    Implement replace command

--- main.rb
+++ main.rb
@@ -1,10 +1,23 @@
 require 'discordrb'
 
-Discordrb::Commands::CommandBot.new token: File.read('bot'), prefix: '!'
+TOKEN = File.read('config')
+bot = Discordrb::Bot.new token: TOKEN
 
-bot.command()
-bot.message(with_text: 'Ping!') do |event|
-  event.respond 'Pong!'
+bot.message(starting_with: '!!s') do |event|
+  messages = JSON.parse(Discordrb::API::Channel.messages(TOKEN, event.channel.id, 100).body)
+  message = event.message.content.split(' ').first
+  parts = message.split('/', -1)
+
+  regex = /#{Regexp.quote(parts[1])}/
+  target = messages.find { |m| m['content'].match(regex) && m['id'] != event.message.id }
+  if target
+    if parts[3] === 'g'
+      target['content'].gsub! regex, parts[2]
+    else
+      target['content'].sub! regex, parts[2]
+    end
+    event.respond target['content']
+  end
 end
 
 bot.run
