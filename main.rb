0428e004eec54286b0dd882641781607d65e1e5a
Joshua LeBlanc <josh@grep.sh>
2018-05-03 09:22:30 -0300

    Dice command

--- main.rb
+++ main.rb
@@ -2,9 +2,9 @@ require 'discordrb'
 require 'rbnacl/libsodium'
 require 'google/apis/customsearch_v1'
 require_relative './commands/replace_command'
-require_relative './commands/google_image_command'
 require_relative './commands/image_search'
 require_relative './commands/github'
+require_relative './commands/dice_command'
 include Commands
 
 
@@ -21,5 +21,6 @@ bot.message(starting_with: '!!image') do |event|
   ImageSearch.new(CONFIG['google'], CONFIG['search_id']).run!(event)
 end
 bot.message(starting_with: '!!github', &github_command)
+bot.message(starting_with: '!!dice', &dice_command)
 bot.send_message('346409372152496138-439726621130358795', 'Hello, world!')
 bot.run
