57a027051686eb8f7272e5e0fd85fc3709351950
Josh LeBlanc <joshleblanc94@gmail.com>
2018-05-04 11:33:24 -0300

    Update replace_reaction.rb
    regex includes flags

--- reactions/replace_reaction.rb
+++ reactions/replace_reaction.rb
@@ -13,7 +13,11 @@ module Reactions
           messages = JSON.parse(Discordrb::API::Channel.messages(CONFIG['discord'], event.channel.id, 100).body)
           message = event.message.content
           parts = message.split('/', -1)
-          regex = /#{parts[1]}/
+          options = 0
+          options += Regexp::IGNORECASE if parts[3].include? 'i'
+          options += REGEXP::MULTILINE if parts[3].include? 'm'
+          options += REGEXP::EXTENDED if parts[3].include? 'x'
+          regex = Regexp.new(parts[1], options)
           target = messages.find { |m| m['content'].match(regex) && m['id'].to_i != event.message.id }
           if target
             if parts[3] === 'g'
