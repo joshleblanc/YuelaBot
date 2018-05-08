b5e65e72a4423a6c5db374c47c1fd01614619b42
Joshua LeBlanc <jleblanc@omics.com>
2018-05-07 21:53:46 -0300

    Added info command

---
+++ commands/info_command.rb
@@ -0,0 +1,33 @@
+module Commands
+  class InfoCommand
+    class << self
+      def name
+        [:i, :info]
+      end
+
+      def attributes
+        {
+          min_args: 1,
+          max_args: 1,
+          description: 'i[nfo] [learned command name]'
+        }
+      end
+
+      def command
+        lambda do |_, *args|
+          command = UserCommand.first(name: args[0])
+          if command
+            sio = StringIO.new
+            sio << "Command #{command.name}\n"
+            sio << "Input: #{command.input}\n"
+            sio << "Output: #{command.output}\n"
+            sio << "created by #{command.creator} on #{command.created_at.strftime("%F")}"
+            sio.string
+          else
+            "Command #{args[0]} does not exist"
+          end
+        end
+      end
+    end
+  end
+end
