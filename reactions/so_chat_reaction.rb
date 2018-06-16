5ba7bd2d767f96a84e4f6ae8ec53e355f87f8401
Josh LeBlanc <joshleblanc94@gmail.com>
2018-06-16 18:29:37 -0500

    Don't take adjacent messages, and onebox if the post is an image.

--- reactions/so_chat_reaction.rb
+++ reactions/so_chat_reaction.rb
@@ -19,15 +19,15 @@ module Reactions
           url = match_data[0]
 
           transcript = Nokogiri::HTML(open(url))
-          container = transcript.at_css('div.highlight').parent.parent
+          message = transcript.at_css('div.highlight')
+          container = message.parent.parent
           user = container.at_css('div.username').children[0]
           avatar = container.at_css('div.avatar').at_css('img').attr('src')
-          message = container.css('div.content').map do |el|
-            el.text.strip
-          end.join("\n")
+          image = container.css('div.content img')
           room = transcript.at_css('span.room-name a')
           embed = Embed.new(title: room.text)
-          embed.description = message
+          embed.image = EmbedImage.new(url: image.attr('src')) unless image.empty?
+          embed.description = message.css('.content').text
           embed.color = '123123'.to_i(16)
           embed.url = "http://chat.stackoverflow#{room.attr('href')}"
           embed.author = EmbedAuthor.new(name: user.text, icon_url: avatar, url: "https://chat.stackoverflow.com#{user.attr('href')}")
