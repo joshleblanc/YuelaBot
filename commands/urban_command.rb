b033bb1140a19e787bc949906df97539ceffc3a2
Joshua LeBlanc <josh@grep.sh>
2018-06-17 09:35:23 -0300

    initial implementation of urban command

---
+++ commands/urban_command.rb
@@ -0,0 +1,39 @@
+module Commands
+  class UrbanCommand
+    class << self
+      include Discordrb::Webhooks
+
+      def name
+        :urban
+      end
+
+      def attributes
+        {
+            min_args: 1,
+            usage: 'urban term',
+            description: 'Return the urban dictionary definition for a term'
+        }
+      end
+
+      def command
+        lambda do |event, *term|
+          term = term.join ' '
+          response = RestClient.get('http://api.urbandictionary.com/v0/define', params: { term: term })
+          body = JSON.parse response
+          definition = body['list'].first
+          if definition
+            embed = Embed.new(
+                title: definition['word'],
+                description: definition['definition'],
+                url: definition['permalink'],
+                author: EmbedAuthor.new(name: definition['author']),
+                timestamp: definition['written_on'],
+                fields: [EmbedField.new(name: 'Example', value: definition['example'])]
+            )
+          end
+
+        end
+      end
+    end
+  end
+end
