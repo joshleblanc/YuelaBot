d49e82bb425e80dfd55d7b5fc4bd69481968b6ab
Joshua LeBlanc <joshleblanc94@gmail.com>
2018-08-15 15:55:47 +0000

    Added ndugger command

---
+++ commands/ndugger_command.rb
@@ -0,0 +1,23 @@
+module Commands
+    class NduggerCommand
+        class << self
+            def name
+                [:ndugger]
+            end
+
+            def attributes
+                {
+                    description: 'Duggerfy a message',
+                    usage: 'ndugger [message]'
+                }
+            end
+
+            def command
+                lambda do |event, *args|
+                    message = args.join(' ')
+                    message.chars.map { |c| rand > 0.5 ? c.upcase : c }.join
+                end
+            end
+        end
+    end
+end
