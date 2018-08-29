0bb5a0ff68b220e02c8d159adfe5df09add155a2
Joshua LeBlanc <joshleblanc94@gmail.com>
2018-08-29 21:36:03 +0000

    Added bible command

---
+++ commands/bible_command.rb
@@ -0,0 +1,30 @@
+module Commands
+    class BibleCommand
+        class << self
+        include Discordrb::Webhooks
+            def name
+                [:bible]
+            end
+
+            def attributes
+                {
+                    usage: "bible [book] [chapter:verse(s)].\nExamples: romans 3:2-5\nromans 3:2-5,13:5,12-5",
+                    description: "Gets bible verse(s) from a given book"
+                }
+            end
+
+            def command
+                lambda do |e, book, *query|
+                    req = RestClient.get("http://bible-api.com/#{book}+#{query.join}")
+                    resp = JSON.parse req.body
+                    embed = Embed.new(
+                        title: resp["reference"],
+                        description: resp["text"],
+                        color: "#FF0000"
+                    )
+                    e.respond nil, false, embed
+                end
+            end
+        end
+    end
+end
