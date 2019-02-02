d8bd4cb84e5a532b99d8638fa1bb58ed54f1f3bc
Ben Fortune <benjamin.fortune1@gmail.com>
2019-02-02 00:48:03 +0000

    Use scheduler instead of our own threads

--- commands/remindme_command.rb
+++ commands/remindme_command.rb
@@ -1,53 +1,33 @@
 module Commands
-    class RemindMeCommand
-      class << self
-        def name
-          [:rm, :remind]
-        end
+  class RemindMeCommand
+    class << self
+      def name
+        [:rm, :remind]
+      end
 
-        def regex
-          /(\d+)(s|m|h)?/
-        end
-  
-        def attributes
-          {
-            usage: 'remind [time] [message]',
-            min_args: 2,
-            description: 'Reminds the user after an allocated time has passed',
-            contains: self.regex
-          }
-        end
-  
-        def command
-          lambda do |event, time, *args|
-            message = args.join(' ')
+      def attributes
+        {
+          usage: 'remind [time] [message]',
+          min_args: 2,
+          description: 'Reminds the user after an allocated time has passed',
+        }
+      end
 
-            match_data = time.match(self.regex)
+      def command
+        lambda do |event, time, *args|
+          message = args.join(' ')
 
-            time = match_data[0].to_i
-
-            if match_data[1] then
-              if match_data[1] == 's' then
-                length = time
-              elsif match_data[1] == 'm' then
-                length = time * 60
-              elsif match_data[1] == 'h' then
-                length = time * 3600
-              end
+          begin
+            Rufus::Scheduler.singleton.in time do
+              event.respond "<@#{event.author.id}> #{message}"
             end
-
-            Thread.abort_on_exception = true
-            begin
-              t = Thread.new {
-                sleep time
-                event << message
-                t.kill if t.alive? 
-              }
-            rescue Exception => e
-              event << e.inspect
-            end
+            event.respond "Okay <@#{event.author.id}>, I'll remind you to #{message} in #{time}"
+          rescue Exception => err
+            event.respond err.message
           end
+          nil
         end
       end
     end
   end
+end
