8c077978811789e5df2d469fbbff4b82b823fd71
Josh LeBlanc <jleblanc@omics.com>
2019-02-20 18:54:40 -0400

    added game_key model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_143156) do
+ActiveRecord::Schema.define(version: 2019_02_20_185043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 2019_02_20_143156) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_birthdays_on_user_id"
+  end
+
+  create_table "game_keys", force: :cascade do |t|
+    t.string "name"
+    t.string "key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "role_colors", force: :cascade do |t|
---
+++ models/game_key.rb
@@ -0,0 +1,3 @@
+class GameKey < ApplicationRecord
+
+end
