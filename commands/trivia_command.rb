e376041f37ab9042903a0b96894f602e622033dd
Joshua LeBlanc <me@jleb.ca>
2019-02-23 14:55:55 -0400

    Instantiate trivia command and account for no players

--- commands/trivia_command.rb
+++ commands/trivia_command.rb
@@ -1,9 +1,9 @@
 module Commands
   class TriviaCommand
-    class << self
-      include Discordrb::Webhooks
-      include Discordrb::Events
+    include Discordrb::Webhooks
+    include Discordrb::Events
 
+    class << self
       def name
         :trivia
       end
@@ -17,185 +17,201 @@ module Commands
       end
 
       def command(event)
-        @event = event
-        @scores = {}
-        @max_points = 10
-        @duration = 30
-        @show_hint = false
-        @stop = false
-        loop do
-          @question = get_question
-          @answer = @question['answer'].gsub(/<i>(.+)<\/i>/, "\\1")
-          @winner = nil
-          question_message = send_question
-
-          answer_loop = start_answer_loop
-          time_limit_thread = time_limit_routine(answer_loop, question_message)
-
-          if answer_loop.join.value
-            handle_correct_answer
-          else
-            handle_no_correct_answer
-          end
-          time_limit_thread.terminate
-
-          if done?
-            send_scores
-            break
-          end
-        end
+        trivia = TriviaCommand.new(event)
+        trivia.run
       end
+    end
 
-      private
+    def initialize(event)
+      @event = event
+      @scores = {}
+      @max_points = 10
+      @duration = 30
+      @show_hint = false
+      @stop = false
+    end
 
-      def get_question
-        response = RestClient.get("jservice.io/api/random")
-        result = JSON.parse(response.body)
-        question = result.first
-        if question['invalid_count'] || question['question'].empty?
-          get_question
+    def run
+      loop do
+        @question = get_question
+        @answer = @question['answer'].gsub(/<i>(.+)<\/i>/, "\\1")
+        @winner = nil
+        question_message = send_question
+
+        answer_loop = start_answer_loop
+        time_limit_thread = time_limit_routine(answer_loop, question_message)
+
+        if answer_loop.join.value
+          handle_correct_answer
         else
-          question
+          handle_no_correct_answer
+        end
+        time_limit_thread.terminate
+
+        if done?
+          send_scores
+          break
         end
       end
+    end
 
-      def time_limit_routine(answer_loop, question_message)
-        Thread.new do
-          step = @duration / 3
-          sleep step
-          hint = make_hint
-          question_embed = question_message.embeds[0]
-          new_embed = Embed.new(
-              title: question_embed.title,
-              color: question_embed.color,
-              fields: question_embed.fields.map { |f| EmbedField.new(name: f.name, value: f.value)},
-              footer: EmbedFooter.new(text: hint)
-          )
-          question_message = question_message.edit(nil, new_embed)
 
-          sleep step
-          new_embed.footer = EmbedFooter.new(text: update_hint(hint))
-          question_message.edit(nil, new_embed)
+    private
 
-          sleep step
-          new_embed.footer = EmbedFooter.new(text: update_hint(hint, 1))
-          question_message.edit(nil, new_embed)
-          answer_loop.terminate
-        end
+    def get_question
+      response = RestClient.get("jservice.io/api/random")
+      result = JSON.parse(response.body)
+      question = result.first
+      if question['invalid_count'] || question['question'].empty?
+        get_question
+      else
+        question
       end
+    end
 
-      def send_scores
-        sorted_scores = @scores.sort_by { |_, v| v[:score] }.reverse
-        send_embed do |embed|
-          final_results = StringIO.new
-          sorted_scores.each do |k, v|
-            final_results.puts "**<@#{k}>** got #{v[:score]} points"
-          end
+    def time_limit_routine(answer_loop, question_message)
+      Thread.new do
+        step = @duration / 3
+        sleep step
+        hint = make_hint
+        question_embed = question_message.embeds[0]
+        new_embed = Embed.new(
+            title: question_embed.title,
+            color: question_embed.color,
+            fields: question_embed.fields.map { |f| EmbedField.new(name: f.name, value: f.value)},
+            footer: EmbedFooter.new(text: hint)
+        )
+        question_message = question_message.edit(nil, new_embed)
+
+        sleep step
+        new_embed.footer = EmbedFooter.new(text: update_hint(hint))
+        question_message.edit(nil, new_embed)
+
+        sleep step
+        new_embed.footer = EmbedFooter.new(text: update_hint(hint, 1))
+        question_message.edit(nil, new_embed)
+        answer_loop.terminate
+      end
+    end
+
+    def send_scores
+      sorted_scores = @scores.sort_by { |_, v| v[:score] }.reverse
+      send_embed do |embed|
+        final_results = StringIO.new
+        sorted_scores.each do |k, v|
+          final_results.puts "**<@#{k}>** got #{v[:score]} points"
+        end
+        if final_results.string.empty?
+          embed.fields = [
+              EmbedField.new(name: "Final Scores:", value: "No one played!")
+          ]
+        else
+
           embed.fields = [
               EmbedField.new(name: "Final Scores:", value: final_results.string)
           ]
         end
       end
+    end
 
-      def done?
-        someone_won? || game_stopped?
+    def done?
+      someone_won? || game_stopped?
+    end
+
+    def game_stopped?
+      @stop
+    end
+
+    def someone_won?
+      if @winner
+        return @scores[@winner.id][:score] == @max_points
       end
+      false
+    end
 
-      def game_stopped?
-        @stop
+    def handle_correct_answer
+      @scores[@winner.id] ||= { score: 0 }
+      @scores[@winner.id][:name] = @winner&.name
+      @scores[@winner.id][:score] += 1
+
+      send_embed do |embed|
+        embed.description = "<@#{@winner&.id}> got it! The answer was: **#{@answer}**"
       end
+    end
 
-      def someone_won?
-        if @winner
-          return @scores[@winner.id][:score] == @max_points
-        end
-        false
+    def handle_no_correct_answer
+      send_embed do |embed|
+        embed.description = "Time's up! The answer was: **#{@answer}**"
       end
+    end
 
-      def handle_correct_answer
-        @scores[@winner.id] ||= { score: 0 }
-        @scores[@winner.id][:name] = @winner&.name
-        @scores[@winner.id][:score] += 1
-
-        send_embed do |embed|
-          embed.description = "<@#{@winner&.id}> got it! The answer was: **#{@answer}**"
-        end
-      end
-
-      def handle_no_correct_answer
-        send_embed do |embed|
-          embed.description = "Time's up! The answer was: **#{@answer}**"
-        end
-      end
-
-      def start_answer_loop
-        Thread.new do
-          loop do
-            response = @event.message.await!
-            if response.message.content == '!!stop' && !@stop
-              @stop = true
-              send_stop_message
-            end
-
-            if response.message.content.downcase == CGI.unescapeHTML(@answer).downcase
-              @winner = response.user
-              break
-            end
+    def start_answer_loop
+      Thread.new do
+        loop do
+          response = @event.message.await!
+          if response.message.content == '!!stop' && !@stop
+            @stop = true
+            send_stop_message
           end
-          true
-        end
-      end
 
-      def send_question
-        p @question['category']['title'], CGI.unescapeHTML(@question['question'])
-        send_embed do |embed|
-          embed.fields = [
-              EmbedField.new(name: "Category", value: @question['category']['title']),
-              EmbedField.new(name: "Question", value: CGI.unescapeHTML(@question['question']))
-          ]
-          embed.description = "You can stop trivia with !!stop"
-        end
-      end
-
-      def send_stop_message
-        send_embed do |embed|
-          embed.description = "Okay, stopping after this question"
-        end
-      end
-
-      def make_hint
-        @answer.chars.map do |c|
-          if c == " "
-            "  "
-          elsif rand < 0.10
-            "#{c} "
-          else
-            "_ "
+          if response.message.content.downcase == CGI.unescapeHTML(@answer).downcase
+            @winner = response.user
+            break
           end
-        end.join
-      end
-
-      def update_hint(hint, chance = 0.5)
-        hint = hint.gsub("  ", " ").gsub(/(\w) /, "\\1")
-        hint.chars.map.with_index do |c, i|
-          if c == "_" && rand < chance
-            "#{@answer[i]} "
-          elsif c == " "
-            "  "
-          else
-            "#{c} "
-          end
-        end.join
-      end
-
-      def send_embed
-        @event.channel.send_embed do |embed|
-          embed.title = "Trivia!"
-          embed.color = 0x1c8fe
-          yield embed
         end
-
+        true
       end
+    end
+
+    def send_question
+      p @question['category']['title'], CGI.unescapeHTML(@question['question'])
+      send_embed do |embed|
+        embed.fields = [
+            EmbedField.new(name: "Category", value: @question['category']['title']),
+            EmbedField.new(name: "Question", value: CGI.unescapeHTML(@question['question']))
+        ]
+        embed.description = "You can stop trivia with !!stop"
+      end
+    end
+
+    def send_stop_message
+      send_embed do |embed|
+        embed.description = "Okay, stopping after this question"
+      end
+    end
+
+    def make_hint
+      @answer.chars.map do |c|
+        if c == " "
+          "  "
+        elsif rand < 0.10
+          "#{c} "
+        else
+          "_ "
+        end
+      end.join
+    end
+
+    def update_hint(hint, chance = 0.5)
+      hint = hint.gsub("  ", " ").gsub(/(\w) /, "\\1")
+      hint.chars.map.with_index do |c, i|
+        if c == "_" && rand < chance
+          "#{@answer[i]} "
+        elsif c == " "
+          "  "
+        else
+          "#{c} "
+        end
+      end.join
+    end
+
+    def send_embed
+      @event.channel.send_embed do |embed|
+        embed.title = "Trivia!"
+        embed.color = 0x1c8fe
+        yield embed
+      end
+
     end
   end
 end
