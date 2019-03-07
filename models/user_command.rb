380e4226d866a37de7b9ddd3b99b55f76d80b9f9
Josh LeBlanc <jleblanc@omics.com>
2019-03-07 13:11:50 -0400

    use the block form of gsub

--- models/user_command.rb
+++ models/user_command.rb
@@ -4,10 +4,13 @@ class UserCommand < ApplicationRecord
             test = args.join(' ')
             if input == '.*' || test.match(/#{input}/)
               response = test.gsub(/#{input}/, output)
-              matches = response.scan /:.+?:(?!\d+>)/
-              matches.each do |m|
+              response.gsub! /:.+?:(?!\d+>)/ do |m|
                 formatted_emoji = BOT.all_emoji.find { |e| e.name == m[1...-1] }
-                response.sub!(m, formatted_emoji.mention) if formatted_emoji
+                if formatted_emoji
+                  formatted_emoji.mention
+                else
+                  m
+                end
               end
               response
             end
