ea5e6d09a11e56ba7236e90089d0541abcfd65ec
Josh LeBlanc <jleblanc@omics.com>
2019-03-07 18:04:46 -0400

    maybe fixed the 4chan command?

--- commands/4chan_command.rb
+++ commands/4chan_command.rb
@@ -1,6 +1,7 @@
 module Commands
   class Random4ChanCommand
     class << self
+      include Discordrb::Webhooks
       def name
         :'4chan'
       end
@@ -15,47 +16,43 @@ module Commands
         }
       end
 
-      def command(event, *args)
-        Random4ChanCommand.new(args[0]).run(event)
+      def command(event, id)
+        board = Fourchan::Kit::Board.new id
+        res = board.posts.sample
+        text, quotes = parse_response(res.com)
+        event.channel.send_embed do |embed|
+          embed.color = '#cc0066'
+          embed.author = EmbedAuthor.new(name: res.name)
+          embed.image = EmbedImage.new(url: res.image_link) unless res.image_link.nil?
+          embed.footer = EmbedFooter.new(text: res.now)
+          embed.description = text unless text.strip.empty?
+          embed.fields = quotes.map do |q|
+            EmbedField.new(**q)
+          end
+        end
       end
-    end
 
-    def initialize(board)
-      @board = Fourchan::Kit::Board.new board
-    end
-
-    def run(event)
-      res = @board.posts(1).sample
-      embed = Discordrb::Webhooks::Embed.new(color: '#cc0066')
-      author = Discordrb::Webhooks::EmbedAuthor.new(name: res.name)
-      image = Discordrb::Webhooks::EmbedImage.new(url: res.image_link)
-      footer = Discordrb::Webhooks::EmbedFooter.new(text: res.now)
-      text = res.com
-      if text
-        text = text.gsub('<br>', "\n")
+      def parse_response(text)
+        return "", [] unless text
+        text.gsub!('<br>', "\n")
         text = text.split(/<a href=".*" class="quotelink">&gt;&gt;.*\/a>/).delete_if(&:empty?).first
-        quotes = text.scan(/<span class="quote">&gt;.*?<\/span>/)
+        quotes = text.scan(/<span class="quote">&gt;.*?<\/span>/) rescue []
         num = quotes.length
-        quotes.flatten.each do |quote|
+        quotes = quotes.flatten.map do |quote|
           text.delete!(quote)
           value = quote.sub(/<span class="quote">&gt;(.*)<\/span>/, Nokogiri::HTML.parse("\\1").text)
           if value.length > 1024
             value = "#{value[0..1020]}..."
           end
-          embed.add_field(name: '>' * num, value: value)
           num -= 1
+          {name: '>' * (num + 1), value: value}
         end
         value = Nokogiri::HTML.parse(text).text
         if value.length > 2048
           value = "#{value[0..2044]}..."
         end
-        embed.description = value unless text.strip.empty?
+        [value, quotes]
       end
-      embed.image = image unless image.url.nil?
-      embed.author = author
-      embed.footer = footer
-      event.respond nil, false, embed
-      nil
     end
   end
 end
