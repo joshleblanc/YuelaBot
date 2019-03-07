a21d3692a800f8b92e90b708fc34ab790aa4af87
Josh LeBlanc <jleblanc@omics.com>
2019-03-07 12:53:16 -0400

    Changed user commands to use external emoji

--- models/user_command.rb
+++ models/user_command.rb
@@ -2,9 +2,20 @@ class UserCommand < ApplicationRecord
     def run
         lambda do |_, *args|
             test = args.join(' ')
-            p test, input, output
             if input == '.*' || test.match(/#{input}/)
-              test.gsub(/#{input}/, output)
+              response = test.gsub(/#{input}/, output)
+              response.split(' ').map do |word|
+                if word.match /:.+?:/
+                  formatted_emoji = BOT.all_emoji.find { |e| e.name == word[1...-1] }
+                  if formatted_emoji
+                    formatted_emoji.mention
+                  else
+                    word
+                  end
+                else
+                  word
+                end
+              end.join(' ')
             end
         end
     end
