455fa05d566c9190fd02da7b7223a056dd3ee7e3
Josh LeBlanc <jleblanc@omics.com>
2019-03-07 16:11:17 -0400

    translation command
    closes #46

---
+++ commands/translations/list_languages_command.rb
@@ -0,0 +1,32 @@
+module Commands
+  class ListLanguagesCommand
+    class << self
+      def name
+        :list_languages
+      end
+
+      def attributes
+        {
+            usage: "list_languages",
+            description: "List languages for the translate command",
+            aliases: [:ll]
+        }
+      end
+
+      def command(_, *_)
+        service = Google::Apis::TranslateV2::TranslateService.new
+        service.key = ENV['google']
+        languages = service.list_languages(target: 'en').languages
+
+        response = StringIO.new
+        response.puts '```'
+        response.printf "%-5s %s\n", "ID", "Name"
+        languages.each do |l|
+          response.printf "%-5s %s\n", l.language, l.name
+        end
+        response.puts '```'
+        response.string
+      end
+    end
+  end
+end
---
+++ commands/translations/translate_command.rb
@@ -0,0 +1,35 @@
+module Commands
+  class TranslateCommand
+    class << self
+
+      def name
+        :translate
+      end
+
+      def attributes
+        {
+            min_args: 3,
+            description: "Translate something to another language. Get available languages with !!list_languages",
+            usage: "translate [source id] [target id] [words]",
+            aliases: [:tr]
+        }
+      end
+
+      def init
+
+      end
+
+      def command(_, source, target, *args)
+        service = Google::Apis::TranslateV2::TranslateService.new
+        service.key = ENV['google']
+        query = args.join(' ')
+        translation = service.list_translations(query, target, source: source).translations.first
+        if translation
+          translation.translated_text
+        else
+          "No translations found"
+        end
+      end
+    end
+  end
+end
