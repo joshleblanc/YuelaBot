aebb790a5447522620c7f70b88d2e07bb4f65866
Josh LeBlanc <jleblanc@omics.com>
2019-03-13 10:31:48 -0300

    remove typo

--- test/urban_command_test.rb
+++ test/urban_command_test.rb
@@ -9,7 +9,7 @@ class UrbanCommandTest < Test::Unit::TestCase
     stub(event).from_bot? { false }
     stub(event).respond { |_, _, c| c }
 
-    urban_response = RestClient.get('httdp://api.urbandictionary.com/v0/define', params: {term: 'test'})
+    urban_response = RestClient.get('http://api.urbandictionary.com/v0/define', params: {term: 'test'})
     definition = JSON.parse(urban_response)['list'].first
 
     result = Commands::UrbanCommand.command(event, "test")
