0db65f32979fe6ff95107d14518ab8337d14427c
Josh LeBlanc <jleblanc@omics.com>
2019-03-15 15:16:20 -0300

    added tests for urban command

--- test/urban_command_test.rb
+++ test/urban_command_test.rb
@@ -7,16 +7,37 @@ require_relative '../bot'
 class UrbanCommandTest < Test::Unit::TestCase
   include Discordrb::Webhooks
 
-  def test_single_argument
-    event = Object.new
-    stub(event).from_bot? { false }
-    stub(event).respond { |_, _, c| c }
+  def setup
+    @event = Object.new
+    stub(@event).from_bot? { false }
+    stub(@event).respond { |_, _, c| c }
+    stub(@event).<< { |c| c }
 
     urban_response = RestClient.get('http://api.urbandictionary.com/v0/define', params: {term: 'test'})
-    definition = JSON.parse(urban_response)['list'].first
+    @urban = JSON.parse(urban_response)['list']
+  end
 
-    result = Commands::UrbanCommand.command(event, "test")
+  def test_single_argument
+    definition = @urban.first
+    result = Commands::UrbanCommand.command(@event, "test")
 
     assert(definition['definition'].eql?(result.description), "expected #{definition['definition']} but was #{result.description}")
+  end
+
+  def test_with_index
+    definition = @urban[1]
+
+    result = Commands::UrbanCommand.command(@event, "test", 1)
+    assert(definition['definition'].eql?(result.description), "expected #{definition['definition']} but was #{result.description}")
+  end
+
+  def test_with_out_of_bounds_index
+    result = Commands::UrbanCommand.command(@event, "test", 99999)
+    assert(result.eql? "I couldn't find anything for that")
+  end
+
+  def test_negative_index
+    result = Commands::UrbanCommand.command(@event, "test", -1)
+    assert(result.eql? "I couldn't find anything for that")
   end
 end
