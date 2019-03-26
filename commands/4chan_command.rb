66801b65599e2a3b92885e0537804800129e61ac
Josh LeBlanc <jleblanc@omics.com>
2019-03-26 15:59:05 -0300

    Fixed the 4chan command incorrectly garbling comments

--- commands/4chan_command.rb
+++ commands/4chan_command.rb
@@ -41,7 +41,7 @@ module Commands
         quotes = text.scan(/<span class="quote">&gt;.*?<\/span>/) rescue []
         num = quotes.length
         quotes = quotes.flatten.map do |quote|
-          text.delete!(quote)
+          text.slice! quote
           value = quote.sub(/<span class="quote">&gt;(.*)<\/span>/, Nokogiri::HTML.parse("\\1").text)
           if value.length > 1024
             value = "#{value[0..1020]}..."
@@ -49,7 +49,9 @@ module Commands
           num -= 1
           {name: '>' * (num + 1), value: value}
         end
+
         value = Nokogiri::HTML.parse(text).text
+        p value
         if value.length > 2048
           value = "#{value[0..2044]}..."
         end
