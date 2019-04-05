e9cd9acc6ee908a63d044db31a422a4191258b2d
Josh LeBlanc <joshleblanc94@gmail.com>
2019-04-05 00:33:09 +0000

    added github comand test

---
+++ test/github_command_test.rb
@@ -0,0 +1,24 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'test/unit/rr'
+require_relative '../bot'
+
+class GithubTest < Test::Unit::TestCase
+    def setup
+        @event = Object.new
+        stub(@event).from_bot? { false }
+    end
+
+    def test_it_ignores_bot
+        stub(@event).from_bot? { true }
+        result = Commands::GithubCommand.command(@event)
+
+        assert_nil result
+    end
+
+    def test_it_works
+        result = Commands::GithubCommand.command(@event)
+        assert_equal "https://github.com/HorizonShadow/YuelaBot", result
+    end
+end
