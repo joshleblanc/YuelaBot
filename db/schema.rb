05cfdb71bc6bc46d7bbdd18b45fec24ad819ac9c
Josh LeBlanc <joshleblanc94@gmail.com>
2019-06-05 21:57:33 +0000

    add banned column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_231236) do
+ActiveRecord::Schema.define(version: 2019_06_05_214206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2019_03_07_231236) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "banned", default: false
   end
 
 end
