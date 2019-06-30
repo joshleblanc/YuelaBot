5fc6d57fecd9e276f5deaf1a38254408e6dab6d0
Josh LeBlanc <joshleblanc94@gmail.com>
2019-06-30 16:02:03 +0000

    make remind command default to minutes

--- commands/remindme_command.rb
+++ commands/remindme_command.rb
@@ -9,7 +9,7 @@ module Commands
         {
             usage: 'remind [time] [message]',
             min_args: 2,
-            description: 'Reminds the user after an allocated time has passed',
+            description: 'Reminds the user after an allocated time has passed. [time] defaults to minutes, but you can specify the unit with "h", "m", "s", etc.',
             aliases: [:rm, :remindme]
         }
       end
@@ -17,6 +17,11 @@ module Commands
       def command(event, time, *args)
         return if event.from_bot?
         message = args.join(' ')
+
+        # Assume the time is coming in as an integer without a duration
+        # If it is, give it a minutes duration, if it's not, pass it through
+        time = "#{Integer(time)}m" rescue time
+
         begin
           Rufus::Scheduler.singleton.in time do
             event.respond "<@#{event.author.id}> #{message}"
