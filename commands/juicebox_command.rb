53856f06e58ddfe04e5f998b7173afab19d4ecb2
Joshua LeBlanc <joshleblanc94@gmail.com>
2019-08-30 00:12:29 +0000

    reindent

--- commands/juicebox_command.rb
+++ commands/juicebox_command.rb
@@ -1,62 +1,62 @@
 module Commands
-    class JuiceboxCommand
-        class << self
-
-            def name
-                :juicebox
+  class JuiceboxCommand
+    class << self
+      
+      def name
+        :juicebox
+      end
+      
+      def attributes
+        {
+          description: "Give someone a juicebox",
+          usage: "!!juicebox",
+          aliases: [:jb]
+        }
+      end
+      
+      def command(event)
+        return if event.from_bot?
+        
+        # Image could be an attachment, or in a rich embed, or in an auto embed
+        image_url = nil
+        event.channel.history(100).each do |message|
+          message.embeds.each do |embed|
+            if embed.type == :image
+              image_url = message.content
+              break
+            elsif embed.type == :rich && embed.image
+              image_url = embed.image.url
+              break
             end
-
-            def attributes
-                {
-                    description: "Give someone a juicebox",
-                    usage: "!!juicebox",
-                    aliases: [:jb]
-                }
+          end
+          break if image_url
+          
+          message.attachments.each do |attachment|
+            if attachment.image?
+              image_url = attachment.url
+              break
             end
-
-            def command(event)
-                return if event.from_bot?
-                
-                # Image could be an attachment, or in a rich embed, or in an auto embed
-                image_url = nil
-                event.channel.history(100).each do |message|
-                    message.embeds.each do |embed|
-                        if embed.type == :image
-                            image_url = message.content
-                            break
-                        elsif embed.type == :rich && embed.image
-                            image_url = embed.image.url
-                            break
-                        end
-                    end
-                    break if image_url
-
-                    message.attachments.each do |attachment|
-                        if attachment.image?
-                            image_url = attachment.url
-                            break
-                        end
-                    end
-                    break if image_url
-                end
-                
-                return "No images found" unless image_url
-                
-                response = RestClient::Request.execute(method: :get, url: "https://juiceboxify.me/api?url=#{CGI.escape(image_url)}", timeout: -1)
-
-                # api returns an error as json if there's na error, otherwise just throws the file at you
-                if response.headers[:content_type] == "application/json"
-                    error = JSON.parse(response)
-                    return error['error']
-                else
-                    file = Tempfile.new(['juicebox', '.jpeg'])
-                    file.binmode
-                    file.write(response.body)
-                    file.rewind
-                    event.send_file file
-                    file.close
-                end               
-            end
+          end
+          break if image_url
         end
+        
+        return "No images found" unless image_url
+        
+        response = RestClient::Request.execute(method: :get, url: "https://juiceboxify.me/api?url=#{CGI.escape(image_url)}", timeout: -1)
+        
+        # api returns an error as json if there's na error, otherwise just throws the file at you
+        if response.headers[:content_type] == "application/json"
+          error = JSON.parse(response)
+          return error['error']
+        else
+          file = Tempfile.new(['juicebox', '.jpeg'])
+          file.binmode
+          file.write(response.body)
+          file.rewind
+          event.send_file file
+          file.close
+        end               
+      end
     end
+  end
 end
