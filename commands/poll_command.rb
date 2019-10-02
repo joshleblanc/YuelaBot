adff1880bdd2ce9d836c2c96bd2109647bd1579a
Lauren <lauren.ball@annkissam.com>
2019-10-02 23:24:46 +0000

    Remove decimal point from hardcoded 0

--- commands/poll_command.rb
+++ commands/poll_command.rb
@@ -97,7 +97,7 @@ module Commands
 
         results = @options.sort_by { |o| o[:votes] }.reverse.map do |o|
           if votes == 0
-            percent = 0.0
+            percent = 0
           else
             percent = ((o[:votes] / votes.to_f) * 100).round
           end
