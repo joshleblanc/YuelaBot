4fbd7c0f6d3a0e3ee60aedb3a6d81b335ad5e6b0
Josh LeBlanc <joshleblanc94@gmail.com>
2019-10-09 11:54:07 +0000

    handle nil cases in escape_md

--- lib/helpers/escape.rb
+++ lib/helpers/escape.rb
@@ -2,6 +2,7 @@ module Helpers
   def escape_md(text)
     # Replace all Discord-flaored markdown special characters, which are not
     # themselves escaped.
+    return "" if text.nil?
     text.gsub(/(?<!\\)([`~*_|>])/) { "\\" + $1 }
   end
 end
