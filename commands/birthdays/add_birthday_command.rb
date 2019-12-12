476371a3ff41c0e194049d5dda76f831801a0a37
Josh LeBlanc <joshleblanc94@gmail.com>
2019-12-12 02:30:39 +0000

    added defaults for add birthday command
    fixes #228

--- commands/birthdays/add_birthday_command.rb
+++ commands/birthdays/add_birthday_command.rb
@@ -7,7 +7,7 @@ module Commands
 
       def attributes
         {
-            min_args: 3,
+            min_args: 1,
             max_args: 3,
             description: "Add a birthday for a user",
             usage: "[add_bday][abd] [@user] [month (Number)] [day (Number)]",
@@ -15,16 +15,25 @@ module Commands
         }
       end
 
-      def command(event, user, month, day)
+      def command(event, user, month = nil, day = nil)
         return if event.from_bot?
         begin
           mention = event.message.mentions.first
-
           if Birthday.find_by(user_id: mention.id, server: event.server.id)
             "A birthday already exists for that user"
           else
             user = User.find_or_create_by(id: mention.id) do |u|
               u.name = mention.name
+            end
+            if month.nil? && day.nil?
+              now = Time.now
+              month = now.month
+              day = now.day
+              event.respond "Defaulting to month: #{month}, day: #{day}. Is this okay? (y/n)"
+              response = event.user.await!
+              return unless response.message.content.downcase.start_with? 'y'
+            elsif day.nil?
+              return "If you provide a month, you must provide a day"
             end
             user.birthdays << Birthday.new(
               month: month.to_i,
