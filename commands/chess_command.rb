ec3fe9a5cb79711f19dc5ee786e4df9f8ee9dfb6
Josh LeBlanc <joshleblanc94@gmail.com>
2019-12-19 19:01:09 +0000

    add chess command

---
+++ commands/chess_command.rb
@@ -0,0 +1,24 @@
+module Commands
+  class ChessCommand
+    class << self
+
+      def name
+        :chess
+      end
+
+      def attributes
+        {
+            usage: "bible",
+            description: "Start a game of chess"
+        }
+      end
+
+      def command(event, book, *query)
+        return if event.from_bot?
+        event << "This is where I would put the URL"
+
+        event << "IF I HAD ONE >:("
+      end
+    end
+  end
+end
