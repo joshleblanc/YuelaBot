c8ba927e31a56460aeb44279cb5b35f71d40a472
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-01-01 22:16:28 -0400

    passing the base url

--- models/so_chat_proxy.rb
+++ models/so_chat_proxy.rb
@@ -1,10 +1,6 @@
 class SoChatProxy < ApplicationRecord
   def listen!
-    so_chat = SoChat.new(channel_id, room_id, ENV['so_user'], ENV['so_pass'])
+    so_chat = SoChat.new(channel_id, room_id, ENV['so_user'], ENV['so_pass'], base_url)
     so_chat.listen!
-  end
-
-  def stop!
-
   end
 end
