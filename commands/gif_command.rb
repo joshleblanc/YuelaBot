70e79430c572c38c84f274993efa5b30e7a886ee
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-02-04 07:57:02 -0400

    gif command
    fixes #268

---
+++ commands/gif_command.rb
@@ -0,0 +1,25 @@
+module Commands
+  class GifCommand
+    class << self
+      def name
+        :gif
+      end
+
+      def attributes
+        {
+          description: "TODO: Describe the command",
+          usage: "TODO: How to use the command",
+          aliases: [:giphy, :tenor, :g]
+        }
+      end
+
+      def command(event, *args)
+        return if event.from_bot?
+
+        response = RestClient.post("https://rightgif.com/search/web",  "text=#{args.join(' ')}")
+        url = JSON.parse(response.body)["url"]
+        url
+      end
+    end
+  end
+end
