d8a01dce831f8c1f1fcd39c34e390a77d3e708fd
Josh LeBlanc <joshleblanc94@gmail.com>
2020-02-25 15:13:40 -0400

    Update pornhub_command.rb

--- commands/pornhub_command.rb
+++ commands/pornhub_command.rb
@@ -15,13 +15,17 @@ module Commands
 
         def command(event, *args)
           return if event.from_bot?
+            
+          if event.channel.nsfw?
+             query = args.join(' ')
 
-          query = args.join(' ')
-
-          resp = open("http://www.pornhub.com/webmasters/search?search=#{query}").read
-          json = JSON.parse(resp)
-          video = json['videos'].first
-          video['url']
+             resp = open("http://www.pornhub.com/webmasters/search?search=#{query}").read
+             json = JSON.parse(resp)
+             video = json['videos'].first
+             video['url'] 
+          else
+             "This command can only be used in a NSFW channel"
+          end
         end
       end
     end
