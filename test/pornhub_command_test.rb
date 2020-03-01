9e78b2ff27c2fda1771b78d16e5f6f8d5fe0bffa
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-03-01 11:06:55 -0400

    fix test

--- test/pornhub_command_test.rb
+++ test/pornhub_command_test.rb
@@ -9,6 +9,23 @@ class PornhubCommandTest < Test::Unit::TestCase
   def setup
     @event = Object.new
     stub(@event).from_bot? { false }
+    stub(@event).channel do
+      channel = Object.new
+      stub(channel).nsfw? { false }
+      channel
+    end
+  end
+
+  def test_nsfw_channel
+    event = Object.new
+    stub(event).from_bot? { false }
+    stub(event).channel do
+      channel = Object.new
+      stub(channel).nsfw? { true }
+      channel
+    end
+    result = Commands::PornhubCommand.command(event, "")
+    assert_not_nil result
   end
 
   def test_it_ignores_bot
@@ -19,6 +36,6 @@ class PornhubCommandTest < Test::Unit::TestCase
 
   def test_it_finds_something
     result = Commands::PornhubCommand.command(@event, "")
-    assert_not_nil result
+    assert_equal "This command can only be used in a NSFW channel", result
   end
 end
