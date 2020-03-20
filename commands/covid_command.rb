130ed2c07d1468df7b0291a5975ae7d6884afc8b
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-03-20 14:58:19 -0300

    list countries if country missing, find country

--- commands/covid_command.rb
+++ commands/covid_command.rb
@@ -15,19 +15,38 @@ module Commands
 
       def command(event, *args)
         return if event.from_bot?
-        country = args.join(' ').titleize
-
         resp = RestClient.get("https://pomber.github.io/covid19/timeseries.json")
         json = JSON.parse(resp.body)
-        data = json[country]&.last
-        return "Not found" unless data
-        <<~RESPONSE
+        countries = json.keys
+        if args.empty?
+          pc = PaginationContainer.new("Valid Countries", countries, 10, event)
+          pc.paginate do |embed, index|
+            left = index * 10
+            right = left + 10
+            embed.description = "```\n"
+            embed.description << countries[left...right].join("\n")
+            embed.description << "\n```"
+          end
+          <<~RESPONSE
           ```
+          #{json.keys.join "\n"}
+          ```
+          RESPONSE
+        else
+          country = args.join(' ').downcase
+          country = countries.find { |c| c.downcase.start_with? country }
+
+          data = json[country]&.last
+          return "Not found" unless data
+          <<~RESPONSE
+          ```
+          #{country}
           confirmed: #{data['confirmed']}
           deaths: #{data['deaths']}
           recovered: #{data['recovered']}
           ```
-        RESPONSE
+          RESPONSE
+        end
       end
     end
   end
