332c44448a5a1c8fbd1145337e42eec65ffb7ec4
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-04-09 15:41:26 -0300

    fix: covid states -a was too long

--- commands/covid_command.rb
+++ commands/covid_command.rb
@@ -18,12 +18,26 @@ module Commands
         event.channel.start_typing
         fixed_args = CSV.parse_line(args.join(' '), col_sep: ' ')
         stdout, stderr, status = Open3.capture3("kovid", *fixed_args)
+
         if status.success?
-          <<~OUTPUT
-            ```
-            #{stdout}
-            ```
-          OUTPUT
+          character_limit = 1992 # 2000 - code block stuff
+          lines = stdout.lines
+          response = []
+          loop do
+
+            next_line = lines.shift
+            if next_line != nil && [next_line, *response].join.size <= character_limit
+              response << next_line
+            else
+              event.respond <<~OUTPUT
+                ```
+                #{response.join}
+                ```
+              OUTPUT
+              response = [next_line]
+            end
+            break if next_line.nil?
+          end
         else
           event.respond <<~OUTPUT
             ```
