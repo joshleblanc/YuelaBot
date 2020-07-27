4657cceea008c9b5830e4524faaee0eb0745dd83
Benjamin Fortune <benjamin.fortune1@gmail.com>
2020-07-27 21:06:42 +0100

    Formatting

--- middleware/check_above.rb
+++ middleware/check_above.rb
@@ -1,7 +1,7 @@
 def CheckAbove(event, *args)
-    check = args.join(' ')
-    if check === '^'
-      args = event.channel.history(1, event.message.id).split(' ')
-    end
-    args
+  check = args.join(' ')
+  if check === '^'
+    args = event.channel.history(1, event.message.id).split(' ')
+  end
+  args
 end
