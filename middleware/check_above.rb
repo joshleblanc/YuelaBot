e8fb782687b79bbc9d196171e5d1829c36f11e5c
Ben Fortune <benjamin.fortune1@gmail.com>
2020-07-28 19:48:51 +0000

    channel.history returns an array

--- middleware/check_above.rb
+++ middleware/check_above.rb
@@ -2,7 +2,7 @@ module Middleware
   def check_above(event, *args)
     check = args.join(' ')
     if check == '^'
-      args = event.channel.history(1, event.message.id).split(' ')
+      args = event.channel.history(1, event.message.id).first.content.split(' ')
     end
     args
   end
