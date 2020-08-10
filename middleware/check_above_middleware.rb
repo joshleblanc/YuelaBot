b6d02880438c1e1c71ce9afb8087257d5eeb36a7
Josh LeBlanc <joshleblanc94@gmail.com>
2020-08-10 20:05:00 +0000

    recursively do replacements

--- middleware/check_above_middleware.rb
+++ middleware/check_above_middleware.rb
@@ -1,13 +1,22 @@
 module Middleware
   class CheckAboveMiddleware < ApplicationMiddleware
-    def before(event, *args)
+
+    def do_replacements(event, args, message = nil)
       args.each_with_index do |a, i|
         match = a.match /^\^+$/
         next if match.nil?
 
         history_index = match[0].size
-        args[i] = event.channel.history(history_index + 1).last.content.split(' ')
+        next_message = event.channel.history(history_index + 1, message).last
+        next_args = next_message.content.split(' ')
+        do_replacements(event, next_args, next_message.id)
+        args[i] = next_args
       end
+      
+    end
+
+    def before(event, *args)
+      do_replacements(event, args, event.message.id)
       args.flatten
     end
   end
