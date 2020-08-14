12ce3dbacceab24cc7fe44b4a59d27d7c53649ca
Josh LeBlanc <joshleblanc94@gmail.com>
2020-08-14 11:02:08 +0000

    add missing file

--- models/user_command.rb
+++ models/user_command.rb
@@ -1,4 +1,13 @@
 class UserCommand < ApplicationRecord
+  def self.can_create?(name)
+    existing_command = Commands.constants.find do |c|
+      command = Commands.const_get(c)
+      command.is_a?(Class) && [command.name, *command.attributes[:aliases]].include?(name.to_sym)
+    end
+    blacklisted_commands = ['help']
+    !existing_command && !blacklisted_commands.include?(name)
+  end
+
   def run(event, *args)
     return if event.from_bot?
 
