33cb8060f97a104a103d6c8dbb6296ec12ef16d6
Josh LeBlanc <joshleblanc94@gmail.com>
2020-12-21 10:24:51 -0400

    fix multiline and extended replacement options

--- reactions/replace_reaction.rb
+++ reactions/replace_reaction.rb
@@ -14,8 +14,8 @@ module Reactions
         parts = message.split(/(?<!\\)\//, -1)
         options = 0
         options += Regexp::IGNORECASE if parts[3].include? 'i'
-        options += REGEXP::MULTILINE if parts[3].include? 'm'
-        options += REGEXP::EXTENDED if parts[3].include? 'x'
+        options += Regexp::MULTILINE if parts[3].include? 'm'
+        options += Regexp::EXTENDED if parts[3].include? 'x'
         regex = Regexp.new(parts[1], options)
         target = messages.find { |m| m.content.match(regex) && m.id.to_i != event.message.id }
         if target
