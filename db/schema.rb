1083ab7640fae429a8831f64de248835a504e63a
Joshua LeBlanc <jleblanc@hey.com>
2021-01-20 22:00:25 +0000

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_122508) do
+ActiveRecord::Schema.define(version: 2021_01_20_165814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,14 @@ ActiveRecord::Schema.define(version: 2021_01_20_122508) do
   create_table "twitch_configs", force: :cascade do |t|
     t.bigint "server"
     t.bigint "channel"
+  end
+
+  create_table "twitch_stream_events", force: :cascade do |t|
+    t.json "data"
+    t.bigint "server"
+    t.integer "twitch_user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "twitch_streams", force: :cascade do |t|
