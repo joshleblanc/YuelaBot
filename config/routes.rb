6b87729f802e77d2c45a3b699f2e297564fb146b
Joshua LeBlanc <jleblanc@hey.com>
2021-01-20 22:17:31 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :webhooks do
-    get :twitch, to: "webhooks/twitch#verify"
-    post :twitch, to: "webhooks/twitch#receive"
+    get :twitch, to: "twitch#verify"
+    post :twitch, to: "twitch#receive"
   end 
 end
