e60c2926c419156320d2ee16108fbd513e982d8e
Joshua LeBlanc <jleblanc@hey.com>
2021-01-21 20:53:27 +0000

    ensure exact username match

--- app/bot/commands/mmr_command.rb
+++ app/bot/commands/mmr_command.rb
@@ -46,6 +46,7 @@ module Commands
         response = RestClient.get("https://www.sc2ladder.com/api/player?query=#{input[0]}&limit=200")
   
         data = JSON.parse(response.body)
+        data.select! { |d| d["username"].downcase == input[0].downcase }
         data.select! { |d| d["race"][0].downcase == race } if race
         data.select! { |d| d["region"][0].downcase == server } if server
         if data.any?
