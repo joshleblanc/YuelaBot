55f8fbe06774dbd970e8c325ff11bafcc14ed14a
Joshua LeBlanc <jleblanc@hey.com>
2021-02-01 14:36:34 +0000

    don't refresh typing event

--- app/bot/middleware/typing_middleware.rb
+++ app/bot/middleware/typing_middleware.rb
@@ -1,23 +1,8 @@
 module Middleware
   class TypingMiddleware < ApplicationMiddleware
-    def initialize
-      @done = false
-    end
-
     def before(event, *args)
-      @thread = Thread.new do
-        loop do
-          break if @done
+      event.channel.start_typing
 
-          event.channel.start_typing
-          sleep 5
-        end
-      end
-      super
-    end
-
-    def after(event, output, *args)
-      @done = true
       super
     end
   end
