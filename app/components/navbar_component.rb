551dbd80f785151c2f4d31ae63f35b399cd78261
Joshua LeBlanc <joshleblanc94@gmail.com>
2021-04-19 07:18:55 -0300

    feat: implement custom theme colors

--- app/components/navbar_component.rb
+++ app/components/navbar_component.rb
@@ -18,11 +18,11 @@ class NavbarComponent < ApplicationComponent
     end
 
     def inactive_classes
-      "text-gray-300 hover:bg-gray-700 hover:text-white"
+      "text-primary-300 hover:bg-primary-700 hover:text-white"
     end
 
     def active_classes
-      "bg-gray-900 text-white"
+      "bg-primary-900 text-white"
     end
 
     render do
@@ -44,12 +44,12 @@ class NavbarComponent < ApplicationComponent
   end
 
   render do
-    div class: "bg-gray-800" do
+    div class: "bg-primary-800" do
       div class: "mx-w-7xl mx-auto px-4 sm:px-6 lg:px-8" do
         div class: "flex items-center justify-between h-16" do
           div class: "flex items-center" do
             div class: "flex-shrink-0" do
-              img class: "h-8 w-8", src: "https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg", alt: "Workflow"
+              h6 "Yuela", class: "text-white"
             end
             div class: "hidden md:block" do
               div class: "ml-10 flex items-baseline space-x-4" do
@@ -59,7 +59,7 @@ class NavbarComponent < ApplicationComponent
           end
           div class: "hidden md:block" do
             div class: "ml-4 flex items-center md:ml-6" do
-              button class: "bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" do
+              button class: "bg-primary-800 p-1 rounded-full text-primary-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary-800 focus:ring-white" do
                 span t(".view-notifications"), class: "sr-only"
                 svg class: "h-6 w-6", xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", aria_hidden: "true" do
                   tag :path, stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "2", d: "M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
@@ -72,7 +72,7 @@ class NavbarComponent < ApplicationComponent
             end
           end
           div class: "-mr-2 flex md:hidden" do
-            button type: "button", class: "bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white", aria_controls: "mobile-menu", aria_expanded: "false" do
+            button type: "button", class: "bg-primary-800 inline-flex items-center justify-center p-2 rounded-md text-primary-400 hover:text-white hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary-800 focus:ring-white", aria_controls: "mobile-menu", aria_expanded: "false" do
               span t(".open-main-menu"), class: "sr-only"
             end
 
