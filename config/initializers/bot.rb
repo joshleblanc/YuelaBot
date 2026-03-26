1c4efc2089ebd5dbe4ff3a4d66514684451e0e49
Joshua LeBlanc <jleblanc@hey.com>
2026-03-26 14:47:53 -0300

    fix: don't load discord bot if there's no token

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -1,15 +1,17 @@
-BOT = Discordrb::Commands::CommandBot.new(
-  token: ENV['DISCORD'],
-  client_id: ENV['DISCORD_CLIENT_ID'],
-  prefix: ->(message) do
-    next unless message&.server
-    prefix = ServerPrefix.where(server: message.server.id).first_or_create do |p|
-      p.prefix = "!!"
-    end
-    message.content[prefix.prefix.size..-1] if message.content.start_with?(prefix.prefix)
-  end,
-  log_level: :debug,
-  parse_self: true
-)
+if ENV["DISCORD"] && ENV["DISCORD_CLIENT_ID"]
 
+  BOT = Discordrb::Commands::CommandBot.new(
+    token: ENV['DISCORD'],
+    client_id: ENV['DISCORD_CLIENT_ID'],
+    prefix: ->(message) do
+      next unless message&.server
+      prefix = ServerPrefix.where(server: message.server.id).first_or_create do |p|
+        p.prefix = "!!"
+      end
+      message.content[prefix.prefix.size..-1] if message.content.start_with?(prefix.prefix)
+    end,
+    log_level: :debug,
+    parse_self: true
+  )
+end
 TEXT_MODEL = "minimax-m25"
